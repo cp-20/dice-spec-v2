b23a516b22a0862fd78780c9d8c10e98819e97e1
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-27 18:42:41 +0900

    🚧 `next/font`を使って Zen Kaku Gothic Antique と M PLUS 1p を簡単に使えるように

---
+++ src/shared/fonts/MPlus1p.ts
@@ -0,0 +1,6 @@
+import { M_PLUS_1p as fontGeneratorMPlus1p } from 'next/font/google';
+
+export const fontMPlus1p = fontGeneratorMPlus1p({
+  weight: ['400', '500', '700'],
+  subsets: ['latin', 'latin-ext'],
+});
---
+++ src/shared/fonts/ZenKakuGothicAntique.ts
@@ -0,0 +1,7 @@
+import { Zen_Kaku_Gothic_Antique as fontGeneratorZenKakuGothicAntique } from 'next/font/google';
+
+export const fontZenKakuGothicAntique = fontGeneratorZenKakuGothicAntique({
+  weight: ['700'],
+  preload: true,
+  subsets: ['latin'],
+});
