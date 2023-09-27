acd42057880f9ce389b7dd4cfdb0adc71013032c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-27 10:14:36 +0900

    🚧 Next.js の Typed Routes を有向に (experimental)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
