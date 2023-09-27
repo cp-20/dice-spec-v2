c6af3b29116c0c404d8d33bb52be931b955b93d4
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-27 16:17:17 +0900

    🚧 全体のフォントを M PLUS 1p に指定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
 import 'ress';
 import '@/app/globals.css';
+import clsx from 'clsx';
+import { M_PLUS_1p as fontMPlus1P } from 'next/font/google';
 import type { FC, ReactNode } from 'react';
 import { GoogleTagManager } from '@/shared/components/GoogleTagManager';
+
+const rootFont = fontMPlus1P({
+  weight: ['400', '500', '700'],
+  subsets: ['latin', 'latin-ext'],
+});
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <>
       <html lang="ja">
-        <body>
+        <body className={clsx('text-slate-700', rootFont.className)}>
           <GoogleTagManager />
 
           {children}
