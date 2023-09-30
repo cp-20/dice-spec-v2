7fa26eb968e7397a2378900c88c22870a6e71cde
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-30 19:38:39 +0900

    🚧 ColorInputで現在選択されている色をわかりやすく表示するように

--- src/app/(app)/ccfolia/_components/ColorInput.tsx
+++ src/app/(app)/ccfolia/_components/ColorInput.tsx
@@ -1,8 +1,10 @@
-import { IconHash, IconUserCircle } from '@tabler/icons-react';
+import { IconCheck, IconHash, IconUserCircle } from '@tabler/icons-react';
+import clsx from 'clsx';
 import type { FC } from 'react';
 import type { FormSchemaType } from '@/app/(app)/ccfolia/_components/InputForm';
 import { Button } from '@/shared/components/ui/button';
 import { Input } from '@/shared/components/ui/input';
+import { checkIsDarkColor } from '@/shared/lib/isDarkColor';
 
 type HexColorString = FormSchemaType['color'];
 
@@ -31,27 +33,27 @@ export const ColorInput: FC<ColorInputProps> = ({ value, onChange }) => {
 
       {/* TODO: 折り返しの時に左右の余白が違う画面幅が存在する */}
       <div className="flex flex-wrap items-center gap-2 rounded-md border p-4">
-        <ColorTip color="#222222" onChange={onChange} />
-        <ColorTip color="#f44336" onChange={onChange} />
-        <ColorTip color="#e91e63" onChange={onChange} />
-        <ColorTip color="#9c27b0" onChange={onChange} />
-        <ColorTip color="#673ab7" onChange={onChange} />
-        <ColorTip color="#3f51b5" onChange={onChange} />
-        <ColorTip color="#2196f3" onChange={onChange} />
-        <ColorTip color="#03a9f4" onChange={onChange} />
-        <ColorTip color="#00bcd4" onChange={onChange} />
-        <ColorTip color="#009688" onChange={onChange} />
-        <ColorTip color="#4caf50" onChange={onChange} />
-        <ColorTip color="#8bc34a" onChange={onChange} />
-        <ColorTip color="#cddc39" onChange={onChange} />
-        <ColorTip color="#ffeb3b" onChange={onChange} />
-        <ColorTip color="#ffc107" onChange={onChange} />
-        <ColorTip color="#ff9800" onChange={onChange} />
-        <ColorTip color="#ff5722" onChange={onChange} />
-        <ColorTip color="#795548" onChange={onChange} />
-        <ColorTip color="#607d8b" onChange={onChange} />
-        <ColorTip color="#9e9e9e" onChange={onChange} />
-        <ColorTip color="#e0e0e0" onChange={onChange} />
+        <ColorTip color="#222222" value={value} onChange={onChange} />
+        <ColorTip color="#f44336" value={value} onChange={onChange} />
+        <ColorTip color="#e91e63" value={value} onChange={onChange} />
+        <ColorTip color="#9c27b0" value={value} onChange={onChange} />
+        <ColorTip color="#673ab7" value={value} onChange={onChange} />
+        <ColorTip color="#3f51b5" value={value} onChange={onChange} />
+        <ColorTip color="#2196f3" value={value} onChange={onChange} />
+        <ColorTip color="#03a9f4" value={value} onChange={onChange} />
+        <ColorTip color="#00bcd4" value={value} onChange={onChange} />
+        <ColorTip color="#009688" value={value} onChange={onChange} />
+        <ColorTip color="#4caf50" value={value} onChange={onChange} />
+        <ColorTip color="#8bc34a" value={value} onChange={onChange} />
+        <ColorTip color="#cddc39" value={value} onChange={onChange} />
+        <ColorTip color="#ffeb3b" value={value} onChange={onChange} />
+        <ColorTip color="#ffc107" value={value} onChange={onChange} />
+        <ColorTip color="#ff9800" value={value} onChange={onChange} />
+        <ColorTip color="#ff5722" value={value} onChange={onChange} />
+        <ColorTip color="#795548" value={value} onChange={onChange} />
+        <ColorTip color="#607d8b" value={value} onChange={onChange} />
+        <ColorTip color="#9e9e9e" value={value} onChange={onChange} />
+        <ColorTip color="#e0e0e0" value={value} onChange={onChange} />
 
         <div className="relative">
           <div className="absolute left-0 top-0 grid h-8 w-8 place-content-center rounded-s-md bg-slate-200">
@@ -71,16 +73,22 @@ export const ColorInput: FC<ColorInputProps> = ({ value, onChange }) => {
 
 type ColorTipProps = {
   color: HexColorString;
+  value: HexColorString;
   onChange: (value: string) => void;
 };
 
-const ColorTip: FC<ColorTipProps> = ({ color, onChange }) => {
+const ColorTip: FC<ColorTipProps> = ({ color, value, onChange }) => {
+  const isDarkColor = checkIsDarkColor(color);
   return (
     <Button
       variant="ghost"
       className="h-8 w-8 p-0 transition-all duration-75 hover:opacity-80 active:scale-90"
       style={{ backgroundColor: color }}
       onClick={() => onChange(color)}
-    />
+    >
+      {value === color && (
+        <IconCheck size="16" className={clsx(isDarkColor && 'text-white')} />
+      )}
+    </Button>
   );
 };
---
+++ src/shared/lib/isDarkColor.ts
@@ -0,0 +1,24 @@
+export const checkIsDarkColor = (color: string): boolean => {
+  const [r, g, b] = hexToRgb(color);
+
+  // https://www.w3.org/TR/AERT/#color-contrast
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  return brightness < 128;
+};
+
+const hexToRgb = (hex: string): [number, number, number] => {
+  // ショートバンド (e.g. #03F) をフルバンド (e.g. #0033FF) に変換
+  const hexValue = hex.replace(
+    /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+    (_m, r, g, b) => r + r + g + g + b + b,
+  );
+
+  const rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexValue);
+
+  if (!rgb) {
+    throw new Error(`Invalid hex value: ${hex}`);
+  }
+
+  return [parseInt(rgb[1], 16), parseInt(rgb[2], 16), parseInt(rgb[3], 16)];
+};
