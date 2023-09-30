69ed63a0e8665892c9cf61909e6ebb713b391df0
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-30 18:57:40 +0900

    🚧 ベースのフォントをNoto Sans JPに & 正しく動くように
    `next/font/google`はタイムアウトの関係でCJKフォントの読み込みができないので`next/font/local`を使って自分で設定した

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,13 @@ import '@/shared/styles/globals.css';
 import clsx from 'clsx';
 import type { FC, ReactNode } from 'react';
 import { GoogleTagManager } from '@/shared/components/GoogleTagManager';
-import { fontMPlus1p } from '@/shared/fonts/MPlus1p';
+import { fontNotoSansJP } from '@/shared/fonts/NotoSansJP';
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <>
       <html lang="ja">
-        <body className={clsx('text-slate-700', fontMPlus1p.className)}>
+        <body className={clsx('text-slate-700', fontNotoSansJP.className)}>
           <GoogleTagManager />
 
           {children}
---
+++ src/shared/fonts/NotoSansJP.ts
@@ -0,0 +1,10 @@
+import localFont from 'next/font/local';
+
+export const fontNotoSansJP = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/NotoSansJP-VariableFont_wght.ttf',
+      style: 'normal',
+    },
+  ],
+});
