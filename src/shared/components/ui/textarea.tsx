b9c64343c930f1ee562e2941ec71de526cf2cc3a
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-30 13:07:54 +0900

    🚧 shadcn/ui のコンポーネント群を追加

---
+++ src/shared/components/ui/textarea.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { cn } from '@/shared/lib/shadcn-utils';
+import styles from '@/shared/styles/pretty-scrollbar.module.css';
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          'flex min-h-[80px] w-full rounded-md border border-solid border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          styles['pretty-scrollbar'],
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Textarea.displayName = 'Textarea';
+
+export { Textarea };
