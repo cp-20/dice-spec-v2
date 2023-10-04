6a8a1adf6a6fa6e5c32d8e472eaf120daf34e9e6
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-10-04 16:36:48 +0900

    🔧 vitestにcloseToを導入したり、良い感じに

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom', // jsdomの代わりにhappy-domを設定した
-    setupFiles: [],
+    setupFiles: ['src/test/setup.ts'],
     coverage: {
       provider: 'v8',
     },
     restoreMocks: true, // restore mocks before every tests run
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
