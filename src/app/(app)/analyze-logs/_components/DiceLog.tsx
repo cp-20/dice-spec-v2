3a15f12a8f2144f111a347db078b66502fa0b1e2
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-10-08 16:10:09 +0900

    🚧 DiceLogのスタイルを微修正

--- src/app/(app)/analyze-logs/_components/DiceLog.tsx
+++ src/app/(app)/analyze-logs/_components/DiceLog.tsx
@@ -18,8 +18,8 @@ export const DiceLog: FC<DiceLogProps & ComponentProps<'div'>> = ({
   <div
     className={twMerge(
       'text-sm',
-      log.variant === 'success' && 'text-blue-600',
-      log.variant === 'failed' && 'text-red-700',
+      log.variant === 'success' && 'text-blue-500',
+      log.variant === 'failed' && 'text-red-600',
       className,
     )}
     {...props}
