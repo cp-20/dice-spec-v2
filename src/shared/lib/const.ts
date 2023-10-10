36de59fceb7a1b81ba4e77f704c2dc50c39bfefb
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-10-10 20:04:14 +0900

    🔖 v2.0.0-beta1

--- src/shared/lib/const.ts
+++ src/shared/lib/const.ts
@@ -1,4 +1,5 @@
-export const appVersion = '2.0.0';
+export const appVersion = '2.0.0-beta1';
+
 export const bcdiceApiEndpoint =
   process.env.NEXT_PUBLIC_BCDICE_API_ENDPOINT ??
   'https://bcdice.onlinesession.app';
