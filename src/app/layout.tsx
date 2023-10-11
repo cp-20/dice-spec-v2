554e7813291ecb2242c72480f11437676ce57cb7
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-10-12 03:26:01 +0900

    ✨ Web Vitalsを計測してGAに送信するように

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '@/shared/styles/globals.css';
 import clsx from 'clsx';
 import type { FC, ReactNode } from 'react';
 import { GoogleTagManager } from '@/shared/components/GoogleTagManager';
+import { WebVitals } from '@/shared/components/elements/WebVitals';
 import { fontNotoSansJP } from '@/shared/fonts/NotoSansJP';
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
@@ -11,6 +12,7 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
       <html lang="ja">
         <body className={clsx('text-slate-700', fontNotoSansJP.className)}>
           <GoogleTagManager />
+          <WebVitals />
 
           {children}
         </body>
