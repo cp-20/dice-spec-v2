2e215cae6ff47485d90342c3d8d4d1f22b423518
cp-20 <n.naoki.1587@gmail.com>
2025-01-02 17:21:43 +0900

    ヒストグラムっぽいやつのラベル表示を改善

--- src/app/[locale]/(app)/analyze-logs/_components/hooks/ccfoliaLogAnalysis/resultAggregator.ts
+++ src/app/[locale]/(app)/analyze-logs/_components/hooks/ccfoliaLogAnalysis/resultAggregator.ts
@@ -3,7 +3,12 @@ import { systemStats } from './messageParser';
 
 export const aggregateResults = (results: DiceResultForCharacter['results'], system: System) => {
   const stats = systemStats[system];
-  const labels = stats.pivots.slice(1).map((pivot, i) => `${stats.pivots[i]}-${pivot - 1}`);
+  const labels = stats.pivots.slice(1).map((pivot, i) => {
+    const min = stats.pivots[i];
+    const max = pivot - 1;
+    if (min === max) return `${min}`;
+    return `${min}-${max}`;
+  });
 
   const data = results.reduce((acc, { result }) => {
     const pivot = stats.pivots.findIndex((pivot) => result < pivot) - 1;
