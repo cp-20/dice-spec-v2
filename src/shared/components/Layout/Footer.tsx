96391c732f231819ffb75a3df05eae5f390587da
cp-20 <n.naoki.1587@gmail.com>
2025-02-02 16:31:21 +0900

    2024 -> 2025

--- src/shared/components/Layout/Footer.tsx
+++ src/shared/components/Layout/Footer.tsx
@@ -6,6 +6,6 @@ export const Footer: FC<ComponentProps<'footer'>> = ({ className, ...props }) =>
     className={twMerge('grid place-content-center bg-slate-100 p-2 text-sm text-slate-600', className)}
     {...props}
   >
-    © 2022-2024 cp20
+    © 2022-2025 cp20
   </footer>
 );
