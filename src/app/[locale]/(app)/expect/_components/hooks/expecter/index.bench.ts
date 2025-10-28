49c631941c6c0764a08aef57b9b70603a4b8a15e
cp-20 <n.naoki.1587@gmail.com>
2025-10-28 21:20:45 +0900

    fix: bench & stylelint

--- src/app/[locale]/(app)/expect/_components/hooks/expecter/index.bench.ts
+++ src/app/[locale]/(app)/expect/_components/hooks/expecter/index.bench.ts
@@ -1,13 +1,13 @@
-import { bench } from 'bun:test';
+import { bench, run } from 'mitata';
 import { diceExpecter } from '.';
 
-describe('通常のダイスの計算式', () => {
-  bench('1d6', () => void diceExpecter('1d6'));
-  bench('1d6 + 1d6 + 3', () => void diceExpecter('1d6 + 1d6 - 3'));
-  bench('1d6 * 1d6 * 2 + 5', () => void diceExpecter('1d6 * 1d6 * 2 + 5'));
-  bench('3d6 + 1d100 + 1', () => void diceExpecter('3d6 + 1d100 + 1'));
-  bench('6 - 1d6', () => void diceExpecter('6-1d6'));
-  bench('3 + 5d >= 15', () => void diceExpecter('3 + 5d >= 15'));
-  bench('3 + 5d <= 14', () => void diceExpecter('3 + 5d <= 14'));
-  bench('1d6 >= 8', () => void diceExpecter('1d6 >= 8'));
-});
+bench('1d6', () => void diceExpecter('1d6'));
+bench('1d6 + 1d6 + 3', () => void diceExpecter('1d6 + 1d6 - 3'));
+bench('1d6 * 1d6 * 2 + 5', () => void diceExpecter('1d6 * 1d6 * 2 + 5'));
+bench('3d6 + 1d100 + 1', () => void diceExpecter('3d6 + 1d100 + 1'));
+bench('6 - 1d6', () => void diceExpecter('6-1d6'));
+bench('3 + 5d >= 15', () => void diceExpecter('3 + 5d >= 15'));
+bench('3 + 5d <= 14', () => void diceExpecter('3 + 5d <= 14'));
+bench('1d6 >= 8', () => void diceExpecter('1d6 >= 8'));
+
+await run();
