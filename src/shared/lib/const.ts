d213a6c33d32a9fcb8edb6636d53b2346a5b9a19
cp-20 <n.naoki.1587@gmail.com>
2025-10-29 13:59:46 +0900

    v2.3.5

--- src/shared/lib/const.ts
+++ src/shared/lib/const.ts
@@ -1,3 +1,3 @@
-export const appVersion = '2.3.4';
+export const appVersion = '2.3.5';
 
 export const bcdiceApiEndpoint = process.env.NEXT_PUBLIC_BCDICE_API_ENDPOINT ?? 'https://bcdice.onlinesession.app';
