a54cbc854bde5ec0c519eb7a35d6fa3b8820e8a0
cp-20 <n.naoki.1587@gmail.com>
2025-11-01 18:02:05 +0900

    Sentry で localhost を弾くように

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -25,6 +25,8 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  denyUrls: [/localhost/],
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
