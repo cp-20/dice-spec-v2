778847b63902cfa8865862ba57f5266883e0c7e3
cp-20 <n.naoki.1587@gmail.com>
2026-03-23 19:54:41 +0900

    fix stripe config

--- src/shared/lib/stripe/config.ts
+++ src/shared/lib/stripe/config.ts
@@ -1,5 +1,4 @@
 export const stripeConfig = {
-  publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? '',
   secretKey: process.env.STRIPE_SECRET_KEY ?? '',
   webhookSecret: process.env.STRIPE_WEBHOOK_SECRET ?? '',
   priceIds: {
