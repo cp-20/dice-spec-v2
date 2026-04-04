373890b636262ecfdca0aee398609a2f4b2e2729
cp-20 <n.naoki.1587@gmail.com>
2026-04-05 00:12:17 +0900

    解析結果詳細ページが nav path 判定されないバグを修正

--- src/shared/lib/navigation.ts
+++ src/shared/lib/navigation.ts
@@ -20,7 +20,7 @@ const navLinksRegex = new RegExp(`^(${navLinks.map((link) => link.href).join('|'
 
 export const isNavPath = (path: string): path is NavPaths => navLinksRegex.test(path);
 
-export const specialPageLinks = ['/profile'] as const;
+export const specialPageLinks = ['/profile', '/analyze-logs/[a-zA-Z0-9]+'] as const;
 
 export type SpecialPagePaths = (typeof specialPageLinks)[number];
 
