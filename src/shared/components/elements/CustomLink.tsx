c0e7505c7330c35c34f30ba9d15d318d446747c9
cp-20 <n.naoki.1587@gmail.com>
2026-09-12 00:12:34 +0900

    旧アプリ引継ぎ実装を削除

--- src/shared/components/elements/CustomLink.tsx
+++ src/shared/components/elements/CustomLink.tsx
@@ -1,20 +1,7 @@
-'use client';
-
-import { lazy, type FC, Suspense } from 'react';
-
-import { isOldApp } from '@/shared/lib/const';
+import type { FC } from 'react';
 
 import { ProgressLink, type ProgressLinkProps } from './NavigationProgress';
 
 export type CustomLinkProps = Omit<ProgressLinkProps, 'href'> & { href: string };
 
-const LegacyCustomLink = lazy(() => import('./LegacyCustomLink').then((mod) => ({ default: mod.LegacyCustomLink })));
-
-export const CustomLink: FC<CustomLinkProps> = (props) =>
-  isOldApp ? (
-    <Suspense fallback={<ProgressLink {...props} />}>
-      <LegacyCustomLink {...props} />
-    </Suspense>
-  ) : (
-    <ProgressLink {...props} />
-  );
+export const CustomLink: FC<CustomLinkProps> = (props) => <ProgressLink {...props} />;
