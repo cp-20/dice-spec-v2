e620320ced92b3ee2d982164ccd8d77dc026b5e4
cp20 <47262658+cp-20@users.noreply.github.com>
2026-09-12 21:48:50 +0900

    fix: ページ単独の描画でも翻訳を初期化する (#434)

--- src/shared/i18n/server-layout.tsx
+++ src/shared/i18n/server-layout.tsx
@@ -1,15 +1,10 @@
-import * as i18n from 'i18next';
 import type { ResourceLanguage } from 'i18next';
 import type { FC, ReactNode } from 'react';
 
 import { i18nextInitOptions } from '@/locales/i18next';
 import { i18nConfig, type Locale } from '@/shared/i18n/config';
 
-i18n.init(i18nextInitOptions, (err) => {
-  if (err) {
-    console.error('i18next failed to initialize', err);
-  }
-});
+import { i18n } from './server';
 
 export const wrapRootLayout = (RootLayout: FC<{ children: ReactNode; locale: Locale; resource: ResourceLanguage }>) => {
   const WrappedRootLayout: FC<{
